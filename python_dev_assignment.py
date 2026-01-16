b2b4036339cb1f3e0de720fbb5153354fa298662
Shruti Bansal <156642961+Shruti-84@users.noreply.github.com>
2026-01-16 17:00:00 +0530

    update

--- python_dev_assignment.py
+++ python_dev_assignment.py
@@ -1,56 +1,35 @@
-
-morse_keys = [
-    "._", "_...", "_._.", "_..", ".", ".._.", "__.", "....", "..",
-    ".___", "_._", "._..", "__", "_.", "___", ".__.", "__._", "._.",
-    "...", "_", ".._", "..._", ".__", "_.._", "_.__", "__.."
-]
-
-letters = [
-    "A","B","C","D","E","F","G","H","I","J","K","L","M","N",
-    "O","P","Q","R","S","T","U","V","W","X","Y","Z"
-]
-
+morse_dict = {
+    "._": "A", "_...": "B", "_._.": "C", "_..": "D", ".": "E",
+    ".._.": "F", "__.": "G", "....": "H", "..": "I", ".___": "J",
+    "_._": "K", "._..": "L", "__": "M", "_.": "N", "___": "O",
+    ".__.": "P", "__._": "Q", "._.": "R", "...": "S", "_": "T",
+    ".._": "U", "..._": "V", ".__": "W", "_.._": "X", "_.__": "Y",
+    "__..": "Z"
+}
 encrypted = input("Enter encrypted Morse string: ")
-
 results = []
-
- 
-def get_length(s):
+def get_length(text):
     count = 0
-    for _ in s:
-        count += 1
+    for _ in text:
+        count = count + 1
     return count
-
-
-def match_string(text, pattern, index):
+def match_morse(text, morse, index):
     i = 0
-    while i < get_length(pattern):
+    while i < get_length(morse):
         if index + i >= get_length(text):
             return False
-        if text[index + i] != pattern[i]:
+        if text[index + i] != morse[i]:
             return False
-        i += 1
+        i = i + 1
     return True
-
-
-def decode(index, current):
+def decode(index, current_word):
     if index == get_length(encrypted):
-        results.append(current)
+        results.append(current_word)
         return
-
-    i = 0
-    while i < 26:
-        morse = morse_keys[i]
-        letter = letters[i]
-
-        if match_string(encrypted, morse, index):
-            decode(index + get_length(morse), current + letter)
-        i += 1
-
-
+    for morse in morse_dict:
+        if match_morse(encrypted, morse, index):
+            decode(index + get_length(morse), current_word + morse_dict[morse])
 decode(0, "")
-
-
 n = get_length(results)
 i = 0
 while i < n:
@@ -60,12 +39,10 @@ while i < n:
             temp = results[j]
             results[j] = results[j + 1]
             results[j + 1] = temp
-        j += 1
-    i += 1
-
-
-print("\nPossible Decodings:\n")
+        j = j + 1
+    i = i + 1
+print("All Possible Decodings:\n")
 i = 0
 while i < n:
     print(results[i])
-    i += 1
+    i = i + 1
